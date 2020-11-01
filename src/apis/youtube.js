526353c68aef85d780b0cef710e7625538f6c033
moksh <moksh@source.one>
2020-11-01 21:29:53 +0530

    basic app to be refactored added

---
+++ src/apis/youtube.js
@@ -0,0 +1,12 @@
+import axios from "axios";
+const KEY = "AIzaSyCGZIlVxmFqf4aZNWYUPlROHNxoieuGZqY";
+
+export default axios.create({
+  baseURL: "https://www.googleapis.com/youtube/v3",
+  params: {
+    part: "snippet",
+    type: "video",
+    maxResults: 7,
+    key: KEY,
+  },
+});
---
+++ src/components/VideoList.js
@@ -0,0 +1,13 @@
+import React from "react";
+import VideoItem from "./VideoItem";
+
+const VideoList = ({ videos, onVideoSelect }) => {
+  const videoItems = videos.map((v) => {
+    return (
+      <VideoItem key={v.id.videoId} onVideoSelect={onVideoSelect} video={v} />
+    );
+  });
+  return <div className="ui relaxed divided list">{videoItems}</div>;
+};
+
+export default VideoList;
